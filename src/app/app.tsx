a0cc9049912cef1a10df90973c13e5dffdc4d540
Mahmoud Fares <mahmoudfaresce@gmail.com>
2026-01-05 13:39:35 +0200

    chore: :art: custom import alias (@)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
    return (
       <div className='flex h-screen flex-col items-center justify-center'>
          <h1 className='text-3xl font-bold underline'>Hello There! 👋</h1>
       </div>
    );
 }
-
-export default App;
