f2331c5bfdbb6baf4a6e4b2d048c8ba2d2ae694d
Mahmoud Fares <mahmoudfaresce@gmail.com>
2026-01-05 12:01:23 +0200

    chore: :art: prettier & lint rules config

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-   return (
-      <div className="h-screen w-screen flex flex-col items-center justify-center">
-         <h1 className="text-3xl font-bold underline">Hello There! 👋</h1>
-      </div>
-   );
-}
-
-export default App;
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+   return (
+      <div className='flex h-screen flex-col items-center justify-center'>
+         <h1 className='text-3xl font-bold underline'>Hello There! 👋</h1>
+      </div>
+   );
+}
+
+export default App;
